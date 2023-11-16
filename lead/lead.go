f2ee13e3f29fc252589b875d2b97335428333bd2
ciper <anmolkarki101@gmail.com>
2023-11-16 15:09:59 +0545

    style: Enhance code readability with added comments

--- lead/lead.go
+++ lead/lead.go
@@ -7,28 +7,34 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+// Lead represents the structure of a lead in the CRM system.
 type Lead struct {
 	gorm.Model
-	Name    string `json:"name"`
-	Company string `json:"company"`
-	Email   string `json:"email"`
-	Phone   int    `json:"phone"`
+	Name    string `json:"name"`    // Name of the lead
+	Company string `json:"company"` // Company associated with the lead
+	Email   string `json:"email"`   // Email address of the lead
+	Phone   int    `json:"phone"`   // Phone number of the lead
 }
 
+// GetLeads retrieves all leads from the database and responds with a JSON array.
 func GetLeads(c *fiber.Ctx) {
 	db := database.DBConn
 	var leads []Lead
 	db.Find(&leads)
 	c.JSON(leads)
 }
+
+// GetLead retrieves a specific lead by ID from the database and responds with a JSON object.
 func GetLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
 	var lead Lead
 	db.Find(&lead, id)
 	c.JSON(lead)
-
 }
+
+// NewLead creates a new lead by parsing the request body and adding it to the database.
+// Responds with the created lead as a JSON object.
 func NewLead(c *fiber.Ctx) {
 	db := database.DBConn
 	lead := new(Lead)
@@ -39,6 +45,8 @@ func NewLead(c *fiber.Ctx) {
 	db.Create(&lead)
 	c.JSON(lead)
 }
+
+// DeleteLead deletes a lead by ID from the database and responds with a success message.
 func DeleteLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
--- main.go
+++ main.go
@@ -1,19 +1,53 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
 
+	"github.com/KarkiAnmol/go-fiber-crm/database"
+	"github.com/KarkiAnmol/go-fiber-crm/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// SetUpRoutes configures the routes for the CRM application.
 func SetUpRoutes(app *fiber.App) {
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
 }
-func initDB() {
 
+// initDB initializes the database connection and performs migrations.
+func initDB() {
+	var err error
+
+	// Open a connection to the SQLite database
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	fmt.Println("Connection opened to database")
+
+	// Auto-migrate the Lead model to create the necessary table
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrateds")
 }
+
 func main() {
+	// Create a new Fiber application
 	app := fiber.New()
+
+	// Initialize the database connection and perform migrations
+	initDB()
+
+	// Configure routes for the CRM application
 	SetUpRoutes(app)
+
+	// Start the Fiber application on port 3000
 	app.Listen(3000)
 
+	// Defer closing the database connection until the main function exits
+	defer database.DBConn.Close()
 }
